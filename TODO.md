a5e66a0897e5107076d9efa69b54202b7d794489
mdrahiem <mdrahiem@gmail.com>
2023-02-25 09:51:29 +0100

    Project scope, theme styles and meta tags added

---
+++ TODO.md
@@ -0,0 +1,11 @@
+#### Phase I (Frontend)
+
+- [ ] Add styles
+- [ ] Add non-authenticated routes
+- [ ] Add components
+- [ ] Add authentication
+- [ ] Add SEO
+
+#### Phase II (Fullstack)
+
+- [ ] Implement the backend with Supabase
--- app/root.tsx
+++ app/root.tsx
@@ -10,7 +10,7 @@ import {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Remix realworld example app",
   viewport: "width=device-width,initial-scale=1",
 });
 
@@ -18,8 +18,28 @@ export default function App() {
   return (
     <html lang="en">
       <head>
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="robots" content="index, follow" />
+        <meta key="googlebot" name="googlebot" content="index,follow" />
+        <meta name="google" content="notranslate" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="keywords" content="Remix, realworld" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:site_name" content="remix-realworld" />
+        <meta property="og:title" content="Remix realworld example app" />
         <Meta />
         <Links />
+        <link rel="stylesheet" href="//demo.productionready.io/main.css" />
+        <link
+          rel="stylesheet"
+          href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic&display=swap"
+        />
       </head>
       <body>
         <Outlet />
