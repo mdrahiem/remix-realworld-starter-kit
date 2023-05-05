a8753bb95bd4a738c109ea6ee86c3ac55a5eae93
mdrahiem <mdrahiem@gmail.com>
2023-05-05 12:12:35 +0200

    debugging the redirect in index.tsx

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,5 +2,5 @@ import { redirect } from "@remix-run/node";
 
 export function loader() {
   // Redirecting the / route to /articles
-  return redirect("/articles");
+  throw redirect("/hello-world");
 }
